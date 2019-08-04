83004f7a887e0959dc4e5a7d27e16ae4659a17a4
Vaibhav Desai <vaibhavdesai16@gmail.com>
2019-08-04 11:53:03 +0530

    linear time and constant space algo

--- Challenge3/challege3/src/main/java/dailychallenge/App2.java
+++ Challenge3/challege3/src/main/java/dailychallenge/App2.java
@@ -1,33 +1,42 @@
 package dailychallenge;
 
 /**
- * App2
+ * using linear time and constant space algorithm
  */
 public class App2 {
     public static void main(String[] args) {
-        int[] arr = {3, 4, -1, 1};
+        int[] arr = {-1, -2, 5, 1, 3, 2, 4};
+        //1, 2, /4, 1, 1, 3
         missingPositive(arr);
     }
 
     public static void missingPositive(int[] arr){
-        int max = 0;
-        int min = Integer.MAX_VALUE;
-        int total = 0;
-        int count = 1;
-        for(int num: arr){
-            if(num > 0){
-                if(num > max)
-                    max = num;
-                else if(num < min)
-                    min = num;
-                    
-                count++;
-                total += num;
+        //moving all non positive numbers to left
+        int j = 0;
+        for(int i=0; i<arr.length; i++){
+
+            if(arr[i] <= 0){
+                int temp;
+                temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+                j++;
             }
         }
 
-        int missingnum = (((max + min)*count)/2) - total;
-        System.out.println(missingnum);
+        //take each value and make value at that index is negative
+        for(int i=j; i<arr.length; i++){
+            int value = arr[i];
+            if(arr[Math.abs(value)] > 0){
+                arr[Math.abs(value)] = arr[Math.abs(value)] * -1;
+            }       
+        }
+
+        //print first postive value
+        for(int i=0; i<arr.length; i++){
+            if(arr[i] > 0)
+                System.out.println(i);
+        }
     }
 
 
