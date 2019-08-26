b886b0566261c1b32a97bf90795f12a4caf480d5
Vaibhav Desai <vaibhavdesai16@gmail.com>
2019-08-26 17:41:15 +0530

    xor

--- challenge5/src/main/java/dailycodingchallenge/App.java
+++ challenge5/src/main/java/dailycodingchallenge/App.java
@@ -1,5 +1,7 @@
 package dailycodingchallenge;
 
+import dailycodingchallenge.XORLinkedList.Node;
+
 /**
  * Hello world!
  *
@@ -8,6 +10,37 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        XORLinkedList xorLinkedList = new XORLinkedList();
+        //creating nodes 
+        Node first = new Node(1);
+        xorLinkedList.head = first;
+
+        Node secound = new Node(2);
+        Node third = new Node(3);
+        Node fourth = new Node(4);
+        Node fifth = new Node(5);
+        Node sixth = new Node(6);
+
+        //add object to map to imitate pointer
+        xorLinkedList.addObjectToMap(00000, null);
+        xorLinkedList.addObjectToMap(10001, first);
+        xorLinkedList.addObjectToMap(10002, secound);
+        xorLinkedList.addObjectToMap(10003, third);
+        xorLinkedList.addObjectToMap(10004, fourth);
+        xorLinkedList.addObjectToMap(10005, fifth);
+        xorLinkedList.addObjectToMap(10006, sixth);
+
+        // link nodes to each other 
+        first.pre_next_address = 00000 ^ 10002;
+        secound.pre_next_address = 10001 ^ 10003;
+        third.pre_next_address = 10002 ^ 10004;
+        fourth.pre_next_address = 10003 ^ 10005;
+        fifth.pre_next_address = 10004 ^ 10006;
+        sixth.pre_next_address = 10006 ^ 00000;
+
+        //print all
+        Node node = xorLinkedList.head;
+
+
     }
 }
--- challenge5/src/main/java/dailycodingchallenge/XORLinkedList.java
+++ challenge5/src/main/java/dailycodingchallenge/XORLinkedList.java
@@ -1,44 +1,26 @@
 package dailycodingchallenge;
 
 import java.util.HashMap;
-import java.util.Random;
 
+/**
+ * XORLinkedList
+ */
 public class XORLinkedList {
-    Node lastNode;
-    HashMap<Integer, Node> pointer_mapping = new HashMap<Integer, Node>();
-    Random rad = new Random();
+    Node head;
+    HashMap<Integer, Node> pointermap = new HashMap<Integer, Node>();
 
-    XORLinkedList(){
-        pointer_mapping.put(0, null);
-    }
-
-     static class Node{
-        int xoraddress;
+    public static class Node {
+        int pre_next_address; 
         int data;
 
-        Node(int data){
-        this.data = data;
+        public Node(int data){
+            this.data = data;
         }
     }
 
-    public void add(int data){
-        //created new node
-        Node current_node = new Node(data);
-        pointerFunction(rad.nextInt(), current_node);
-
-        //calculate xor for last node
-        Node previousOfLast = getNodByPointer(lastNode.xoraddress);
-        lastNode.xoraddress = previousOfLast.xoraddress ^ current_node.xoraddress;
-    
+    public void  addObjectToMap(int address, Node node){
+        pointermap.put(address, node);
     }
 
-    public void pointerFunction(int pointer, Node node){
-        //this fucntion will take care of object and pointer mapping
-        pointer_mapping.put(pointer, node);
-    }
-
-    public Node getNodByPointer(int pointer){
-        return pointer_mapping.get(pointer);
-    }
     
 }
